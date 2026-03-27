81a3d2090f0dde8aecac475fef2ac1ff1c83650c
Sagar <m.shafiulalamsagar@gmail.com>
2026-03-28 01:13:22 +0600

    Initial release of ScalerVerse frontend

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  // Tell Tailwind to scan all HTML files in the project for utility classes
+  content: ["./*.{html,js}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
